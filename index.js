3ae5adbf8748bfd560ed18c62b6f1e10eaac2607
Developer Sujeet <157199958+developersujeet@users.noreply.github.com>
2025-01-07 12:42:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 const fs = require("fs");
 const server = http.createServer(app);
 const io = socketIO(server);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const usersFile = path.join(__dirname, "users.json");
 
 app.use(bodyParser.json());
